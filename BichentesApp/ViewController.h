4ac2b4753311a2854c051cd17b8c7080b7ac7fa9
xabita <mutskxabaa@gmail.com>
2016-05-07 08:24:38 -0500

    Initial Commit

---
+++ BichentesApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BichentesApp
+//
+//  Created by Elizabeth Martínez Gómez on 07/05/16.
+//  Copyright © 2016 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BichentesApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BichentesApp
+//
+//  Created by Elizabeth Martínez Gómez on 07/05/16.
+//  Copyright © 2016 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
